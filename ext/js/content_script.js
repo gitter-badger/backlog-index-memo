781f6e2d23c46d38907c346bcd2a23681d922179
nichijo <nichijochan@gmail.com>
2014-06-22 02:50:28 +0900

    最低限の動作

---
+++ ext/js/content_script.js
@@ -0,0 +1,52 @@
+var pageTags = [];
+
+var hostUrl = location.protocol + "//" +  location.hostname;
+var splitPath = location.pathname.split("/");
+var projectName = splitPath[splitPath.length-1];
+var projectViewUrl = hostUrl + "/view/" + projectName
+
+$(document).ready(function(){
+
+    var build = "";
+
+    if( localStorage[projectName] !== undefined ){
+	pageTags = JSON.parse(localStorage[projectName]);
+	console.log( pageTags );
+
+
+	for( i = 0; i<pageTags.length; i++ ){
+	    console.log( pageTags[i] );
+	    build += "<li>" + pageTags[i] + "</li>";
+	}
+    }
+
+    $("#projectNav").after(
+	"<div>"+
+	    "<ul id=\"tagit\" class=\"tagit ui-widget ui-widget-content ui-corner-all\">"+
+	    build +
+	    "</ul>"+
+	"</div>"
+    );
+
+    var isInit = true;
+
+    $('#tagit').tagit({
+	beforeTagAdded: function (event, ui){
+	    if( isInit ) return;
+	    console.log( "push-" + ui.tagLabel );
+	    pageTags.push(ui.tagLabel);
+	    localStorage[projectName] = JSON.stringify(pageTags);
+	    console.log( localStorage[projectName] );
+	},
+	onTagClicked: function (event, ui){
+	    if( isInit ) return;
+	    var showPath = projectViewUrl + "-" + ui.tagLabel;
+	    location.href = showPath;
+	}
+    });
+
+    isInit = false;
+
+});
+
+
